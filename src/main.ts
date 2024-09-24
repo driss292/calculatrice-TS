8f25f57433b46baa94cbfa23c856b3b2df1658ed
driss292 <drisskaci@outlook.fr>
2024-09-24 16:24:59 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const keys = [...document.querySelectorAll(".keys")] as HTMLElement[];
+const displayInput = document.querySelector(
+  ".display .input"
+) as HTMLInputElement;
+const displayOutput = document.querySelector(
+  ".display .output"
+) as HTMLInputElement;
+
+let input = "";
+
+keys.forEach((key: HTMLElement) => {
+  const value = key.dataset.key;
+  key.addEventListener("click", () => {
+    if (value === "clear") {
+      input = "";
+      displayInput.innerHTML = "";
+      displayOutput.innerHTML = "";
+    } else if (value === "backspace") {
+      input = input.slice(0, -1);
+    }
+  });
+});
